f368e6900fde4cb9156f5465c0b62fd5acb588e1
Sansii <sanbuddhacharyas>
2019-03-04 14:06:23 +0545

    After writing only short discrition of make_dataset.py

--- make_dataset.py
+++ make_dataset.py
@@ -6,7 +6,7 @@ def make_image_dataset(image_path):
     # np.random.seed(0)
     images =[]
     #for i in range(num_group):
-    data  = sorted(glob.glob(image_path+'/*.png'))
+    data  = sorted(glob.glob(image_path+'/*.png')) # ./png if file format is in png if file format is in .jpg replace it with .jpg
     for j in data:
         k = cv2.imread(j, cv2.IMREAD_COLOR)
         k =cv2.cvtColor(k ,cv2.COLOR_RGB2BGR)
@@ -15,9 +15,9 @@ def make_image_dataset(image_path):
         img_data = np.array(list(flipped_image.getdata()), np.uint8)
         images.append(img_data)
 
-    np.save("full_working.npy", images)
+    np.save("full_working.npy", images) #give location and Name of the file to save .npy file format
 
     return images
 
 
-make_image_dataset('tero image ko path hal')
+make_image_dataset('tero image ko path hal')#give location of your images where is is saved
